b9769bcb0fc178d07987c3a920187a21ed85b718
Aryan Dhawan <aryansdhawan@gmail.com>
2022-06-11 16:47:19 -0400

    Add files via upload

---
+++ Roll or die/main.py
@@ -0,0 +1,14 @@
+import random
+
+dice_values = [1, 2, 3, 4, 5, 6]
+balence = 0
+
+while True:
+    current = random.choice(dice_values)
+    choice = input(f' Your balence is {balence} would you like to add {current}?[y][n]\t')
+
+    if choice == 'y':
+        balence += current
+
+    if balence > 30:
+        print('You lose!')
