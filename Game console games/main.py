3cd962b384654540dcbca16bc4704663e8a79a01
Aryan Dhawan <aryansdhawan@gmail.com>
2022-06-12 16:43:19 -0400

    Add files via upload

---
+++ Game console games/main.py
@@ -0,0 +1,19 @@
+
+
+print(' Click the links to access the resources')
+
+if input('Do you like Apex Legends?:[y]\t') == 'y':
+    print('https://www.youtube.com/watch?v=GRfbmNm4pEQ')
+
+if input('Do you like War zone?:[y]\t') == 'y':
+    print('https://www.youtube.com/watch?v=ncGOrOo8LdQ')
+
+if input('Do you like Splatoon?:[y]\t') == 'y':
+    print('https://www.youtube.com/watch?v=LrU9wjI5fkA')
+
+if input('Do you like Music?:[y]\t') == 'y':
+    print('https://www.youtube.com/watch?v=xvZqHgFz51I')
+
+if input('Do you like Panda 3?:[y]\t') == 'y':
+    print('https://www.youtube.com/watch?v=n9T06kglhjc')
+
