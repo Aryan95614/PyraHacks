514d7b939b40f63cb2daf92745e8f5b76b34776e
Aryan Dhawan <aryansdhawan@gmail.com>
2022-06-12 13:31:20 -0400

    Add files via upload

---
+++ GuessTheNumber/main.py
@@ -0,0 +1,19 @@
+from random import choice
+import sys
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+score = 5
+
+while True:
+    Random = choice(numbers)
+    thing = int(input(f'Hello, a random number was chosen, please type in a number, your score is {score}:\t'))
+    if thing == Random:
+        score += 1
+        print(f'You win, your score is {score}, the number was {Random}')
+
+    else:
+        print(f'You lose, your score is {score}')
+
+    if score == 5:
+        print('Congrats, now the other part of the game, now you can select the numbers, split by comma:\t')
+        numbers = input(' Numbers: \t').split(', ')
