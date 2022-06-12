2271a7dde691f175768a815964b1d097b55e1829
Aryan Dhawan <aryansdhawan@gmail.com>
2022-06-12 14:40:47 -0400

    Update main.py

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -3,18 +3,18 @@ import sys
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 score = 5
-thing = False
+things = False
 while True:
     Random = choice(numbers)
     thing = int(input(f'Hello, a random number was chosen, please type in a number, your score is {score}:\t'))
-    if thing == Random:
+    if thing == int(Random):
         score += 1
         print(f'You win, your score is {score}, the number was {Random}')
 
     else:
         print(f'You lose, your score is {score}')
 
-    if score == 5 and not thing:
-        thing = True
+    if score == 5 and not things:
+        things = True
         print('Congrats, now the other part of the game, now you can select the numbers, split by comma:\t')
         numbers = input(' Numbers: \t').split(', ')
