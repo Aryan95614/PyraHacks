bfd07578d8d560d3da5709bea86bed3393b5aac0
Aryan Dhawan <aryansdhawan@gmail.com>
2022-06-12 16:52:02 -0400

    Add files via upload

---
+++ Textbook Provider/main.py
@@ -0,0 +1,26 @@
+
+print('Textbook links--------------')
+
+score = 0
+
+choice = input('Do you want a math textbook?[y][n]\t')
+
+if choice == 'y':
+    print('Please click https://openstax.org/details/books/calculus-volume-3')
+    print('Click https://www.khanacademy.org/math/calculus-1 for more info')
+
+
+choice1 = input("Do you want a english textbook?[y][n]\t")
+
+if choice1 == 'y':
+    print('Please click https://www.amazon.ca/IELTS-Reading-GT-2022-Latest/dp/B09K27ZJGP/ref=zg_bs_2223265011_2/130-1436111-7048765?pd_rd_i=B09K27ZJGP&psc=1')
+    print('Click https://www.amazon.ca/Princeton-Review-Premium-Prep-2023/dp/0593450582/ref=asc_df_0593450582/?tag=googleshopc0c-20&linkCode=df0&hvadid=578934847616&hvpos=&hvnetw=g&hvrand=7604890651240827705&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9000844&hvtargid=pla-1641637427661&psc=1 for more info')
+
+
+choice2 = input("Do you want a psychology textbook??[y][n]\t")
+
+if choice2 == 'y':
+    print('Please click https://open.umn.edu/opentextbooks/textbooks/introduction-to-applied-statistics-for-psychology-students')
+    print(' Click https://openpress.usask.ca/introtoappliedstatsforpsych/ for more info')
+
+
